1b29c5594b23337a7d09236d6edf908dfebaa62d
kyrmyzy02 <torekeldykyrmyzy@gmail.com>
2020-12-17 13:56:34 +0600

    drawer

---
+++ app/src/main/java/com/e/menu_with/nav_drawer.java
@@ -0,0 +1,103 @@
+package com.e.menu_with;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.e.menu_with.ui.gallery.GalleryFragment;
+import com.e.menu_with.ui.home.HomeFragment;
+import com.e.menu_with.ui.slideshow.SlideshowFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class nav_drawer extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_drawer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_cam, R.id.nav_cal, R.id.nav_calen
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.bringToFront();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Toast.makeText(getApplicationContext(), "Home is selected", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.nav_cam:
+                        Toast.makeText(getApplicationContext(), "Group is selected", Toast.LENGTH_LONG).show();
+                        Intent group = new Intent(nav_drawer.this, HomeFragment.class);
+                        startActivity(group);
+                        break;
+                    case R.id.nav_cal:
+                        Toast.makeText(getApplicationContext(), "Calls is selected", Toast.LENGTH_LONG).show();
+                        Intent calls = new Intent(nav_drawer.this, GalleryFragment.class);
+                        startActivity(calls);
+                        break;
+                    case R.id.nav_calen:
+                        Toast.makeText(getApplicationContext(), "Setting is selected", Toast.LENGTH_LONG).show();
+                        Intent setting = new Intent(nav_drawer.this, SlideshowFragment.class);
+                        startActivity(setting);
+                        break;
+                }
+                drawer.closeDrawers();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
